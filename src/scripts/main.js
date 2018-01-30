098295d72572bee5ac61b4d69e59fe8630cc01f7
a-pandey <spanit007@outlook.com>
2018-01-30 17:35:23 +0530

    uncommented service-worker script

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -84,13 +84,13 @@
         'https://twitter.com/intent/tweet?text=@amritpandeyy '+$(this).val());
     })
 
-    // if ('serviceWorker' in navigator) {
-    //     navigator.serviceWorker.register('/sw.js')
-    //     .then(function(registration) {
-    //       console.log('Registration successful, scope is:', registration.scope);
-    //     })
-    //     .catch(function(error) {
-    //       console.log('Service worker registration failed, error:', error);
-    //     });
-    // }
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/sw.js')
+        .then(function(registration) {
+          console.log('Registration successful, scope is:', registration.scope);
+        })
+        .catch(function(error) {
+          console.log('Service worker registration failed, error:', error);
+        });
+    }
 })(jQuery);
