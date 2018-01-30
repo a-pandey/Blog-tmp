08fba82712edca244a7de49c128459b995bae832
a-pandey <spanit007@outlook.com>
2018-01-30 16:51:22 +0530

    template article removed

--- blog/article-entry.js
+++ blog/article-entry.js
@@ -4,11 +4,5 @@ var articleListData = [
         'description': '',
         'link': 'january18/how-to-create-your-own-blog-with-html-css/',
         'date': '30 January 2018'
-    },
-    {
-        'heading': 'This is a template article',
-        'description': 'Some template description to template article. That\'s it!',
-        'link': 'template/',
-        'date': ''
     }
 ];
